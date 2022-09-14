b19851120193bef7f617e641fd152f10c294ea45
peaashmeter <40042317+peaashmeter@users.noreply.github.com>
2022-09-14 19:51:16 +0300

    canvaskit 2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "d7bb33bec2e5f0d22e34c7f29d9e2d97",
-"/": "d7bb33bec2e5f0d22e34c7f29d9e2d97",
-"main.dart.js": "7092ebbf93c6b943584ce408bd024ace",
+"index.html": "626aea378b711f100b59cc1c2cefeb3c",
+"/": "626aea378b711f100b59cc1c2cefeb3c",
+"main.dart.js": "bcea7f91c1df6a7ecd40704c98478875",
 "manifest.json": "4220eb8bbb1e234c294c97841de87102",
 "version.json": "337e0d0a198e17f7e539deacbe7321bb"
 };
